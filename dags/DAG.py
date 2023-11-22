c5bf50ed4a2e621e9b461948a5379ae3d9e0b059
Sriram Reddy <99608414+sriram8113@users.noreply.github.com>
2023-11-21 22:33:46 -0700

    files

---
+++ dags/DAG.py
@@ -0,0 +1,61 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from Scripts.pipeline_v3 import extract, load, transform
+from datetime import datetime, timedelta
+import pandas as pd
+import requests
+import boto3
+import requests
+import io
+import time
+import logging
+from sqlalchemy import create_engine
+
+import sys
+from dotenv import load_dotenv
+import os
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 11, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+dag = DAG('Shelter_Dag_cloud',
+          default_args=default_args,
+          description='DAG which pulls, transforms and loads data into a cloud RDS',
+          schedule_interval=timedelta(days=1))
+
+
+retrieve_data_task = PythonOperator(
+    task_id='extract',
+    python_callable=extract,
+    dag=dag)
+
+
+
+transform_data_task = PythonOperator(
+    task_id='transform',
+    python_callable=transform,
+    dag=dag)
+
+
+load_data_task = PythonOperator(
+    task_id='load',
+    python_callable=load,
+    dag=dag)
+
+
+
+retrieve_data_task >> transform_data_task >> load_data_task
+
+
