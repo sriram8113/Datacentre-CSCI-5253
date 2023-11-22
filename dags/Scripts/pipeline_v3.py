91f127f628ff6b523ea6110a280a602da79f0cc7
Sriram Reddy <99608414+sriram8113@users.noreply.github.com>
2023-11-21 22:41:35 -0700

    Update pipeline_v3.py

--- dags/Scripts/pipeline_v3.py
+++ dags/Scripts/pipeline_v3.py
@@ -40,8 +40,8 @@ def extract():
         aws_secret_access_key=YOUR_SECRET_KEY
     )
 
-    # Delete existing data in 'raw_data' folder
-    objects_to_delete = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix='raw_data/')
+    # Delete existing data in 'original_data' folder
+    objects_to_delete = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix='original_data/')
     if 'Contents' in objects_to_delete:
         delete_keys = {'Objects': [{'Key': obj['Key']} for obj in objects_to_delete['Contents']]}
         s3.delete_objects(Bucket=BUCKET_NAME, Delete=delete_keys)
@@ -52,10 +52,10 @@ def extract():
        'outcome_type', 'animal_type', 'sex_upon_outcome', 'age_upon_outcome',
        'breed', 'color', 'outcome_subtype']]
 
-    # Check for existing data in 'old_Data' folder
-    old_data_key = 'old_data/austin_animal_outcomes.csv'
+    # Check for existing data in 'outdated' folder
+    outdated_key = 'outdated/austin_animal_outcomes.csv'
     try:
-        old_obj = s3.get_object(Bucket=BUCKET_NAME, Key=old_data_key)
+        old_obj = s3.get_object(Bucket=BUCKET_NAME, Key=outdated_key)
         df_old = pd.read_csv(io.BytesIO(old_obj['Body'].read()))
     except s3.exceptions.NoSuchKey:
         df_old = pd.DataFrame(columns = ['animal_id', 'name', 'datetime', 'monthyear', 'date_of_birth',
@@ -71,19 +71,19 @@ def extract():
         logging.info("No new or updated data to upload. Creating an empty CSV with headers.")
         df_updated = pd.DataFrame(columns=df_new.columns)
 
-    # Upload data to 'raw_data'
-    logging.info("Uploading data to 'raw_data'...")
+    # Upload data to 'original_data'
+    logging.info("Uploading data to 'original_data'...")
     csv_buffer = io.StringIO()
     df_updated.to_csv(csv_buffer, index=False)
     file_content = csv_buffer.getvalue().encode()
-    s3.put_object(Bucket=BUCKET_NAME, Key='raw_data/austin_animal_outcomes.csv', Body=file_content)
+    s3.put_object(Bucket=BUCKET_NAME, Key='original_data/austin_animal_outcomes.csv', Body=file_content)
 
-    # Update 'Old_Data' with current data
-    logging.info("Updating 'old_data' with current data...")
+    # Update 'outdated' with current data
+    logging.info("Updating 'outdated' with current data...")
     csv_buffer_old = io.StringIO()
     df_new.to_csv(csv_buffer_old, index=False)
     old_file_content = csv_buffer_old.getvalue().encode()
-    s3.put_object(Bucket=BUCKET_NAME, Key=old_data_key, Body=old_file_content)
+    s3.put_object(Bucket=BUCKET_NAME, Key=outdated_key, Body=old_file_content)
 
 
 
@@ -96,7 +96,7 @@ def transform():
         aws_secret_access_key=YOUR_SECRET_KEY
     )
     bucket_name = BUCKET_NAME  # Replace with your S3 bucket name
-    s3_object_name = 'raw_data/austin_animal_outcomes.csv'
+    s3_object_name = 'original_data/austin_animal_outcomes.csv'
 
     # Download the file from S3
     obj = s3.get_object(Bucket=bucket_name, Key=s3_object_name)
@@ -152,22 +152,22 @@ def transform():
     out_buffer = io.StringIO()
     data.drop_duplicates(inplace = True)
     data.to_csv(out_buffer, index=False)
-    s3.put_object(Bucket=bucket_name, Key='cleaned_data/data.csv', Body=out_buffer.getvalue())
+    s3.put_object(Bucket=bucket_name, Key='cleaned/data.csv', Body=out_buffer.getvalue())
 
     out_buffer = io.StringIO()
     animal.drop_duplicates(inplace = True)
     animal.to_csv(out_buffer, index=False)
-    s3.put_object(Bucket=bucket_name, Key='cleaned_data/animal.csv', Body=out_buffer.getvalue())
+    s3.put_object(Bucket=bucket_name, Key='cleaned/animal.csv', Body=out_buffer.getvalue())
   
     out_buffer = io.StringIO()
     outcomes.drop_duplicates(inplace = True)
     outcomes.to_csv(out_buffer, index=False)
-    s3.put_object(Bucket=bucket_name, Key='cleaned_data/outcomes.csv', Body=out_buffer.getvalue())
+    s3.put_object(Bucket=bucket_name, Key='cleaned/outcomes.csv', Body=out_buffer.getvalue())
 
     out_buffer = io.StringIO()
     outcome_events.drop_duplicates(inplace = True)
     outcome_events.to_csv(out_buffer, index=False)
-    s3.put_object(Bucket=bucket_name, Key='cleaned_data/outcomes_events.csv', Body=out_buffer.getvalue())
+    s3.put_object(Bucket=bucket_name, Key='cleaned/outcomes_events.csv', Body=out_buffer.getvalue())
     
 
 
@@ -183,7 +183,7 @@ def load():
     )
 
     # Download data from S3 and load into DataFrames
-    files = ['cleaned_data/animal.csv', 'cleaned_data/outcomes.csv', 'cleaned_data/outcomes_events.csv', 'cleaned_data/data.csv']
+    files = ['cleaned/animal.csv', 'cleaned/outcomes.csv', 'cleaned/outcomes_events.csv', 'cleaned/data.csv']
     dataframes = {}
     for file in files:
         obj = s3.get_object(Bucket=BUCKET_NAME, Key=file)
@@ -193,7 +193,7 @@ def load():
     engine = create_engine(DATABASE_URL)
 
     # Upload DataFrames to PostgreSQL
-    dataframes['cleaned_data/animal.csv'].to_sql('animal', engine, if_exists='append', index=False)
-    dataframes['cleaned_data/outcomes.csv'].to_sql('outcome_type', engine, if_exists='append', index=False)
-    dataframes['cleaned_data/outcomes_events.csv'].to_sql('outcome_event', engine, if_exists='append', index=False)
-    dataframes['cleaned_data/data.csv'].to_sql('fact_table', engine, if_exists='append', index=False)
+    dataframes['cleaned/animal.csv'].to_sql('animal', engine, if_exists='append', index=False)
+    dataframes['cleaned/outcomes.csv'].to_sql('outcome_type', engine, if_exists='append', index=False)
+    dataframes['cleaned/outcomes_events.csv'].to_sql('outcome_event', engine, if_exists='append', index=False)
+    dataframes['cleaned/data.csv'].to_sql('fact_table', engine, if_exists='append', index=False)
